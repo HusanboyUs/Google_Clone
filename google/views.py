d4c13d6243f727ff5c6f4a84d9cd783baee3ad4a
Usmonov Husanboy <70463121+HusanboyUs@users.noreply.github.com>
2021-06-14 19:21:12 +0200

    urls routed

--- google/views.py
+++ google/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homePage(request):
+    return render(request, 'index.html')
+
+def viewPage(request):
+    return render(request, 'view.html')    
